8c649ebfbd35768dd276323b0c044c3994794218
Kevin Azzam <kevin@azz.am>
2018-09-01 01:16:04 +0200

    Add tests for the client
    Refactor some functionality into methods
    Support additional unexpected cases (unexpected acks/resets)
    Validate peer and incoming packets
    Add errbacks to loopingtasks

--- client/sender.py
+++ client/sender.py
@@ -15,18 +15,23 @@ RANDOM_PADDING = [random.randint(0, 127) for x in range(MTU)]
 
 
 class Sender(DatagramProtocol):
-    def __init__(self, jitter, s_port, pad, ip, port, pps):
+    _handshake_retry_period_s = 1
+
+    def __init__(self, jitter, src_port, pad, dst_ip, dst_port, pps):
         super().__init__()
-        self.__counter = 0
+        self._next_packet_id = 0
         self.__jitter = jitter
         self.__pad = pad
-        self.__port = s_port
-        self.__dst_ip = ip
-        self.__dst_port = port
+        self.__port = src_port
+        self.__dst_ip = dst_ip
+        self.__dst_port = dst_port
         self.__pps = pps
         self.__send_task = task.LoopingCall(self.send_next)
+
         self.__handshake_task = task.LoopingCall(self.__send_handshake)
-        self.__handshake_task.start(1, now=False)
+        d = self.__handshake_task.start(
+            self._handshake_retry_period_s, now=False)
+        d.addErrback(self.__send_handshake_errback)
 
     def startProtocol(self):
         host = self.__dst_ip
@@ -41,26 +46,64 @@ class Sender(DatagramProtocol):
 
     def datagramReceived(self, data, info):
         host, port = info
-        data = json.loads(data.decode("ascii"))
+        if host != self.__dst_ip or port != self.__dst_port:
+            self.cleanup()
+            raise ValueError(
+                "received packet from invalid host/port: "
+                "{}:{}".format(host, port) +
+                " - expected {}:{}".format(self.__dst_ip, self.__dst_port))
+
+        try:
+            data = json.loads(data.decode("ascii"))
+        except AttributeError as e:
+            log.error("Couldn't decode packet")
+            self.cleanup()
+            raise e
+
         if data['type'] == "ack":
-            log.info("Handshake confirmed.. starting to send data...")
-            inv_pps = 1 / self.__pps
-            if self.__handshake_task.running:
-                self.__handshake_task.stop()
-            self.__send_task.start(inv_pps, now=True)
+            self.process_handshake_ack()
+
         elif data['type'] == "reset":
-            log.info("Received reset request. Retriggering handshake...")
-            if self.__send_task.running:
-                self.__send_task.stop()
-            self.__counter = 0
-            self.__handshake_task.start(1, now=True)
+            self.process_reset()
+
         elif data['type'] == "abort":
             log.warning("Received 'abort' request, stopping task")
             self.__send_task.stop()
+
         elif data['type'] == "info":
             log.warning("received message: {}".format(data['content']))
+
         else:
             log.error("received unknown data: {}".format(data))
+
+    def process_handshake_ack(self):
+        log.info("Handshake confirmed.. starting to send data...")
+        inv_pps = 1 / self.__pps
+
+        if self.__handshake_task.running:
+            self.__handshake_task.stop()
+            d = self.__send_task.start(inv_pps, now=True)
+            d.addErrback(self.__send_next_errback)
+            return True
+        return False
+
+    def process_reset(self):
+        self._next_packet_id = 0
+
+        if not self.__handshake_task.running:
+            log.info("Received reset request. Retriggering handshake...")
+            self.__send_task.stop()
+            d = self.__handshake_task.start(
+                self._handshake_retry_period_s, now=True)
+            d.addErrback(self.__send_handshake_errback)
+            return True
+
+        else:
+            assert(self.__send_task.running is False)
+            log.warning(
+                "Received reset request while a handshake was pending..."
+                ", ignoring...")
+            return False
 
     def _send_json(self, data, pad):
         encoded = bytes(json.dumps(data), "ascii")
@@ -78,34 +121,51 @@ class Sender(DatagramProtocol):
         else:
             log.debug("Sending {}".format(encoded))
 
+        info = (self.__dst_ip, self.__dst_port)
         if self.__jitter:
             jitter_s = random.randint(0, self.__jitter) / 1000
-            info = (self.__dst_ip, self.__dst_port)
             # jitter helps simulate fake out-of-order packets
             reactor.callLater(jitter_s, self.transport.write, encoded, info)
         else:
             try:
-                info = (self.__dst_ip, self.__dst_port)
                 self.transport.write(encoded, info)
             except AttributeError:
                 # self.transport.connect(self.__dst_ip, self.__dst_port)
                 log.warning("Could not send data...")
 
     def __send_handshake(self):
-        data = {}
-        data['type'] = "handshake"
-        data['padding'] = self.__pad  # unused
-        data['mtu'] = MTU  # unused
-        data['port'] = self.__port  # unused
-        data['pps'] = self.__pps
+        data = {
+            'type': "handshake",
+            'padding': self.__pad,  # unused
+            'mtu': MTU,  # unused
+            'port': self.__port,  # unused
+            'pps': self.__pps,
+        }
         log.info("sending handshake")
         self._send_json(data, pad=False)
 
-    def send_next(self):
-        data = {}
-        data['type'] = "next_packet"
-        data['timestamp'] = time.time()
-        data['packet_id'] = self.__counter
-        self._send_json(data, pad=self.__pad)
+    @staticmethod
+    def __send_handshake_errback(reason):
+        log.error("send_handshake() failed with:")
+        print(reason.getTraceback())
 
-        self.__counter += 1
+    def send_next(self):
+        data = {
+            'type': "next_packet",
+            'timestamp': time.time(),
+            'packet_id': self._next_packet_id,
+        }
+        self._send_json(data, pad=self.__pad)
+        self._next_packet_id += 1
+
+    @staticmethod
+    def __send_next_errback(reason):
+        log.error("send_next() failed with:")
+        print(reason.getTraceback())
+
+    def cleanup(self):
+        if self.__handshake_task.running:
+            self.__handshake_task.stop()
+
+        if self.__send_task.running:
+            self.__send_task.stop()
---
+++ tests/client/test_sender.py
@@ -0,0 +1,108 @@
+import logging
+import json
+
+from twisted.trial import unittest
+
+from client.sender import Sender
+
+
+class MockSender(Sender):
+    def __init__(self):
+        super().__init__(
+            jitter=0,
+            src_port=8080,
+            pad=None,
+            dst_ip="localhost",
+            dst_port=4242,
+            pps=1,
+        )
+
+
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+
+class TestReceivePacket(unittest.TestCase):
+    def setUp(self):
+        self.sender = MockSender()
+        # self.clock = task.Clock()
+        # self.sender.DEFAULT_CLOCK = self.clock
+        logging.disable(logging.DEBUG)  # activate logging
+
+    def tearDown(self):
+        pass
+
+    def receive_packet(self, msg):
+        info = "localhost", 4242
+        encoded = bytes(json.dumps(msg), "ascii")
+        self.sender.datagramReceived(encoded, info)
+
+    # tests
+    def test_receive_bad_packet(self):
+        data = {
+            "type": "ack"
+        }
+        info = "localhost", 4242
+        logging.disable(logging.ERROR)
+        self.assertRaises(
+            AttributeError, self.sender.datagramReceived, data, info)
+
+    def test_receive_expected_ACK(self):
+        data = {
+            "type": "ack"
+        }
+        logging.disable(logging.ERROR)
+        self.receive_packet(data)
+        self.sender.cleanup()
+
+    def test_receive_unexpected_remote(self):
+        data = {
+            "type": "something"
+        }
+        info = "localhost", 4243
+        logging.disable(logging.ERROR)
+        self.assertRaises(
+            ValueError, self.sender.datagramReceived, data, info)
+
+
+class TestReset(unittest.TestCase):
+    def setUp(self):
+        self.sender = MockSender()
+
+    def tearDown(self):
+        self.sender.cleanup()
+
+    def test_unexpected_reset(self):
+        self.assertFalse(self.sender.process_reset())
+        self.assertEquals(self.sender._next_packet_id, 0)
+
+    def test_expected_reset(self):
+        self.sender.process_handshake_ack()
+        self.assertTrue(self.sender.process_reset())
+        self.assertEquals(self.sender._next_packet_id, 0)
+
+
+class TestHandshake(unittest.TestCase):
+    def setUp(self):
+        self.sender = MockSender()
+        logging.disable(logging.DEBUG)
+
+    def tearDown(self):
+        self.sender.cleanup()
+
+    def test_receive_unexpected_ACK(self):
+        logging.disable(logging.ERROR)
+        self.assertTrue(self.sender.process_handshake_ack())
+        self.assertFalse(self.sender.process_handshake_ack())
+
+    def test_ack_after_unexpected_reset(self):
+        logging.disable(logging.ERROR)
+        self.assertFalse(self.sender.process_reset())
+        self.assertTrue(self.sender.process_handshake_ack())
+
+    def test_expected_ACK_after_reset(self):
+        logging.disable(logging.ERROR)
+        self.assertTrue(self.sender.process_handshake_ack())
+        self.assertTrue(self.sender.process_reset())
+        self.assertTrue(self.sender.process_handshake_ack())
